9eedef97f76f425e5dc7ab30ea4db802b455d87a
Damenhua <Damenhua123@gmail.com>
2024-09-22 12:31:33 +0800

    creat searchView.js to control search form

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,6 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js/actual';
 
@@ -15,19 +16,32 @@ const controlRecipe = async function () {
 
     recipeView.renderSpinner();
 
-    // loading recipe
+    // 1) loading recipe
     await model.loadRecipe(id);
     const { recipe } = model.state;
 
-    // Rendering recipe
+    // 2) Rendering recipe
     recipeView.render(recipe);
   } catch (err) {
     recipeView.renderError();
   }
 };
 
+// 3) Searching for results
+const controlSearchResults = async function () {
+  try {
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    model.loadSearchResults(`${query}`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipe);
+  searchView.addHandlerSearch(controlSearchResults)
 };
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -2,13 +2,18 @@ import { API_URL } from './config.js';
 import { getJSON } from './helper.js';
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
     const data = await getJSON(`${API_URL}/${id}`);
-
+    console.log(data);
     let { recipe } = data.data;
+
     state.recipe = {
       id: recipe.id,
       title: recipe.title,
@@ -20,6 +25,25 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
     console.log(state.recipe);
+  } catch (err) {
+    console.error(`${err}`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+    state.search.results = data.data.recipes.map(data => {
+      return {
+        id: data.id,
+        title: data.title,
+        publisher: data.publisher,
+        image: data.image_url,
+      };
+    });
+    console.log(state.search.results);
   } catch (err) {
     console.error(`${err}`);
     throw err;
